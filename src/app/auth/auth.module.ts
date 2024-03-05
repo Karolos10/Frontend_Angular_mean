34e1f2ddd768069ebe882e45d96ca3aaf2abe942
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-03-04 20:09:23 -0500

    form implementation

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
+    LoginPageComponent,
+    RegisterPageComponent,
     AuthLayoutComponent
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
