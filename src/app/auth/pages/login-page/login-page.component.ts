d771878f128e3e5ec54fcd44700d1183f2c59e05
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-03-04 22:48:52 -0500

    implementation of guards and user authentication controls

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
 import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -13,6 +14,7 @@ export class LoginPageComponent {
 
   private fb = inject( FormBuilder );
   private authService = inject( AuthService );
+  private router = inject( Router );
 
   public myForm: FormGroup = this.fb.group({
     email:    ['', [Validators.required, Validators.email]],
@@ -24,7 +26,7 @@ export class LoginPageComponent {
     const { email, password } = this.myForm.value;
 
     this.authService.login( email, password ).subscribe({
-      next: () => console.log('Todo bien'),
+      next: () => this.router.navigateByUrl('/dashboard'),
       error: (message) => {
         Swal.fire('Error', message, 'error');
       }
