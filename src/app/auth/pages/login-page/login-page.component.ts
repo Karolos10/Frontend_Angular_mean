44d7852da2ae288b8d2ead7561dfd17b77cf6957
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-03-04 22:18:12 -0500

    error handling and control

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
+import Swal from 'sweetalert2'
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -21,8 +23,11 @@ export class LoginPageComponent {
 
     const { email, password } = this.myForm.value;
 
-    this.authService.login( email, password ).subscribe( success => {
-      console.log(success);
+    this.authService.login( email, password ).subscribe({
+      next: () => console.log('Todo bien'),
+      error: (message) => {
+        Swal.fire('Error', message, 'error');
+      }
     })
   }
 
