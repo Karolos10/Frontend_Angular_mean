2bc8af5ff31ac9295b29ad7a8ede1949119a58e1
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-03-05 21:01:17 -0500

    implementation of user logout

--- src/app/auth/guards/is-authenticated.guard.ts
+++ src/app/auth/guards/is-authenticated.guard.ts
@@ -15,9 +15,9 @@ export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
     return true;
   };
 
-  if( authService.authStatus() === AuthStatus.checking ){
+  /* if( authService.authStatus() === AuthStatus.checking ){
     return false;
-  }
+  } */
 
   router.navigateByUrl('/auth/login')
   return false;
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
 
 
 @NgModule({
   declarations: [
+    DashboardLayoutComponent
   ],
   imports: [
     CommonModule,
